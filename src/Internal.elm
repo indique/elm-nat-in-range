dfea11857ffbe515b282c6f17aff8d84d7729a74
passi <pascalfeyer@gmail.com>
2021-03-16 18:16:13 +0100

    + toInts | + MinNat

--- src/Internal.elm
+++ src/Internal.elm
@@ -1,5 +1,22 @@
-module Internal exposing (InNat(..))
+module Internal exposing (InNat(..), MinNat(..), inNatToInt, minNatToInt)
+
+{-| Both `InNat` & `MinNat` can use these.
+-}
 
 
 type InNat minimum maximum
     = InNat Int
+
+
+type MinNat min
+    = MinNat Int
+
+
+minNatToInt : MinNat min -> Int
+minNatToInt =
+    \(MinNat int) -> int
+
+
+inNatToInt : InNat min max -> Int
+inNatToInt =
+    \(InNat int) -> int
