0708de23fd1f362ee0b672eb1e48ac461f834120
passi <pascalfeyer@gmail.com>
2021-03-20 13:13:57 +0100

    + dummy module

---
+++ src/Moved.elm
@@ -0,0 +1,14 @@
+module Moved exposing (moved)
+
+{-| A package must contain one module. Never use it.
+
+@docs moved
+
+-}
+
+
+{-| A module must contain a member. Never use it.
+-}
+moved : ()
+moved =
+    ()
